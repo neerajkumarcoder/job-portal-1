42e37097afae05f16b7714eb74bfcc329bccf183
neerajkumarcoder <neerajkumarcbse@gmail.com>
2026-05-01 20:34:28 +0530

    fix error

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://job-portal-1-vmaw.onrender.com", "http://localhost:5173"],
+  origin: ["https://job-portal-1-vmaw.onrender.com"],
   credentials: true,
 };
 
