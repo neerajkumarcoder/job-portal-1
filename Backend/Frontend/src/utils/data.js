64c839b21169e294c2710f5f671e1926c041dfe0
neerajkumarcoder <neerajkumarcbse@gmail.com>
2026-05-01 20:25:04 +0530

    change

--- Backend/Frontend/src/utils/data.js
+++ Backend/Frontend/src/utils/data.js
@@ -1,4 +1,10 @@
-export const USER_API_ENDPOINT = "http://localhost:5011/api/user";
-export const JOB_API_ENDPOINT = "http://localhost:5011/api/job";
-export const APPLICATION_API_ENDPOINT = "http://localhost:5011/api/application";
-export const COMPANY_API_ENDPOINT = "http://localhost:5011/api/company";
+// export const USER_API_ENDPOINT = "http://localhost:5011/api/user";
+// export const JOB_API_ENDPOINT = "http://localhost:5011/api/job";
+// export const APPLICATION_API_ENDPOINT = "http://localhost:5011/api/application";
+// export const COMPANY_API_ENDPOINT = "http://localhost:5011/api/company";
+
+
+export const USER_API_ENDPOINT = "https://job-portal-1-vmaw.onrender.com/api/user";
+export const JOB_API_ENDPOINT = "https://job-portal-1-vmaw.onrender.com/api/job";
+export const APPLICATION_API_ENDPOINT = "https://job-portal-1-vmaw.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://job-portal-1-vmaw.onrender.com/api/company";
--- Backend/index.js
+++ Backend/index.js
@@ -15,19 +15,19 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 //middleware
-app.use(cors({ origin: true, credentials: true }));
+// app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// const corsOptions = {
-//   origin: ["http://localhost:5173"],
-//   credentials: true,
-// };
+const corsOptions = {
+  origin: ["https://job-portal-1-vmaw.onrender.com", "http://localhost:5173"],
+  credentials: true,
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-//api's
+
 
 const _dirname = path.resolve();
 
